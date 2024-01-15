6964509be6fe629a134fcb66d0fd3ebfe6947bca
dgshanee <shaneciardelli27@gmail.com>
2024-01-15 02:33:13 -0600

    Trying out helix

--- main.go
+++ main.go
@@ -8,62 +8,62 @@ import (
 	"os"
 )
 
-type UserPrompt struct{
-	Prompts map[string]string;
+type UserPrompt struct {
+	Prompts map[string]string
 }
 
-var(
-	user UserPrompt;
+var (
+	user UserPrompt
 )
 
-func buildCmd(){
-	buildCmd := flag.NewFlagSet("build", flag.ExitOnError);
-	componentCmd := buildCmd.String("component", "", "Creates a component");
-	dataCmd := buildCmd.String("data", "", "User provided data");
-	IDCmd := buildCmd.String("id", "", "User provided ID");
-	parentCmd := buildCmd.String("parent", "", "Parent of the created component");
+func buildCmd() {
+	buildCmd := flag.NewFlagSet("build", flag.ExitOnError)
+	componentCmd := buildCmd.String("component", "", "Creates a component")
+	dataCmd := buildCmd.String("data", "", "User provided data")
+	IDCmd := buildCmd.String("id", "", "User provided ID")
+	parentCmd := buildCmd.String("parent", "", "Parent of the created component")
 
-	if len(os.Args) < 2{
-		fmt.Println("Invalid command");
-		return;
+	if len(os.Args) < 2 {
+		fmt.Println("Invalid command")
+		return
 	}
 
-	err := buildCmd.Parse(os.Args[2:]);
-	if err != nil{
-		fmt.Println("Error parsing commands");
-		return;
+	err := buildCmd.Parse(os.Args[2:])
+	if err != nil {
+		fmt.Println("Error parsing commands")
+		return
 	}
 
-	user.Prompts = make(map[string]string);
-	user.Prompts["component"] = *componentCmd;
-	user.Prompts["data"] = *dataCmd;
-	user.Prompts["id"] = *IDCmd;
-	user.Prompts["parent"] = *parentCmd;
+	user.Prompts = make(map[string]string)
+	user.Prompts["component"] = *componentCmd
+	user.Prompts["data"] = *dataCmd
+	user.Prompts["id"] = *IDCmd
+	user.Prompts["parent"] = *parentCmd
 }
 
-func main(){
-	dom.LoadComponents();
-	buildCmd();
+func main() {
+	dom.LoadComponents()
+	buildCmd()
 
-	switch os.Args[1]{
+	switch os.Args[1] {
 	case "build":
-		buildComponent();
+		buildComponent()
 	}
 }
 
-func buildComponent(){
-	component, ok := user.Prompts["component"];
-	parent, ok := user.Prompts["parent"];
-	if !ok{
-		fmt.Println("No component specified");
-		return;
+func buildComponent() {
+	component, ok := user.Prompts["component"]
+	parent, ok := user.Prompts["parent"]
+	if !ok {
+		fmt.Println("No component specified")
+		return
 	}
-	switch component{
+	switch component {
 	case "text":
-		var toMarshal components.TextComponent;
-		components.PopulateStruct(&toMarshal, user.Prompts);
-		dom.SaveComponent(&toMarshal, parent);
+		var toMarshal components.TextComponent
+		components.PopulateStruct(&toMarshal, user.Prompts)
+		dom.SaveComponent(&toMarshal, parent)
 	default:
-		panic("Invalid data type");
+		panic("Invalid data type")
 	}
 }
